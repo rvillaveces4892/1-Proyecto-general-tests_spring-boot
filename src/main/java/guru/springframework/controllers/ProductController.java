c20fdc7f44a9f37ea70edfd7c1f03bb54b6c2bab
ramiro <rvillaveces@computec.com>
2019-08-06 13:56:12 -0500

    Prueba jenkins

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -54,7 +54,7 @@ import guru.springframework.utils.Archivos;
 //import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
-//
+
 //import javax.validation.Valid;
 
 /**
